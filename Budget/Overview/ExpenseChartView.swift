b005bfd81627081c8f34f7e03099a1b76ddc11b3
Henry Zhao <henryzhao95@users.noreply.github.com>
2022-12-27 17:08:00 +1100

    Don't show empty categories

--- Budget/Overview/ExpenseChartView.swift
+++ Budget/Overview/ExpenseChartView.swift
@@ -8,7 +8,9 @@ struct ExpenseChartView : View {
     var body: some View {
         if #available(iOS 16.0, *) {
             Chart {
-                ForEach(viewModel.expensesByTime) { expenseDate in
+                ForEach(viewModel.expensesByTime.filter({ expenseDate in
+                    return expenseDate.runningTotal > 0
+                })) { expenseDate in
                     AreaMark(
                         x: .value("Date", expenseDate.date),
                         y: .value("Spent", expenseDate.runningTotal),
@@ -17,14 +19,12 @@ struct ExpenseChartView : View {
                     .foregroundStyle(by: .value("Category", expenseDate.category))
                     
                 }
-                if (viewModel.expensesByTime.count > 1) {
+                if viewModel.expensesByTime.count > 0 {
                     LineMark(x: .value("Date", viewModel.expensesByTime[0].date), y: .value("Spent", 0.0))
-                    LineMark(x: .value("Date", viewModel.expensesByTime[viewModel.expensesByTime.count-1].date), y: .value("Spent", 18.082 * Double(Calendar.current.dateComponents([.day], from: viewModel.expensesByTime[0].date, to: viewModel.expensesByTime[viewModel.expensesByTime.count-1].date).day!)))
+                    LineMark(x: .value("Date", viewModel.expensesByTime[viewModel.expensesByTime.count-1].date), y: .value("Spent", 18.25 * Double(Calendar.current.dateComponents([.day], from: viewModel.expensesByTime[0].date, to: viewModel.expensesByTime[viewModel.expensesByTime.count-1].date).day!)))
                 }
             }
             .padding()
-        } else {
-            // Fallback on earlier versions
         }
     }
 }
